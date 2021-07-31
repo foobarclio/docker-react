a6faaf0e0c83d79a296c2be325fb0e5361881245
Andrii Volosovich <andreyvolosovich@gmail.com>
2021-08-01 01:32:23 +0300

    expose port

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 3000:80
         </a>
       </header>
     </div>
