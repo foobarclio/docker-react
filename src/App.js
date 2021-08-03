f78df73964a610d1012a55f153bc333392199f73
foobarclio <47367778+foobarclio@users.noreply.github.com>
2021-08-04 00:07:22 +0300

    add couple more tests (#1)
    * add couple more tests
    
    * fail tests
    
    * fix tests
    
    Co-authored-by: Andrii Volosovych <andreyvolosovich@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 3000:80
+          Learn React 3000:80 - version 2
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -12,4 +12,9 @@ test('renders learn react link1', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders learn react link2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
